45e1226bb47fa99a52b6ab65ede649a1e7b49878
yalewoo <chongruyuntian@163.com>
2018-03-20 16:32:00 +0800

    add style

---
+++ wp-content/themes/YLW3_lite/404.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" >
+		<title>雅乐网404寻找走失儿童</title>
+		<style type="text/css">
+			body {
+				text-align: center;
+			}
+			iframe {
+				margin-left: auto;
+				margin-right: auto;
+			}
+			h1 {
+				font-size: 30px;
+				font-family: 微软雅黑;
+			}
+		</style>
+	</head>
+	<body>
+		<div id="main">
+			<h1>yalewoo.com 404!</h1>
+			<iframe scrolling='no' frameborder='0' src='http://yibo.iyiyun.com/Home/Distribute/ad404/key/5413' width='654' height='470' style='display:block;'></iframe>
+		</div>
+    <?php get_footer(); ?>
+	</body>
+</html>
+    		
+			
+
+<!-- 
+                            
+--> 
---
+++ wp-content/themes/YLW3_lite/footer.php
@@ -0,0 +1,36 @@
+<footer id="footer">
+      Copyright &copy;   <a title="<?php bloginfo('name') ?>" href="<?php bloginfo('siteurl')?>"><?php bloginfo('name') ?></a>  /<a title="自豪地采用WordPress" href="https://cn.wordpress.org" target="_blank">WordPress</a> / <a title="YLW3.0主题" href="http://www.yalewoo.com/ylw3.html" target="_blank">YLW3.0</a>  /  <a title="老薛主机" href="https://my.laoxuehost.net/aff.php?aff=2518" target="_blank">老薛主机</a>  /  <a title="七牛云存储" href="https://portal.qiniu.com/signup?code=3li1yeb2ph1ea" target="_black">七牛云存储</a>
+
+      <div id="footer_menu">
+      	<?php wp_nav_menu( array( 'theme_location' => 'footer_menu' )); ?>
+      </div>
+      <div id="cnzztongji">
+      	
+
+		<script type="text/javascript">var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");document.write(unescape("%3Cspan id='cnzz_stat_icon_1252889774'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s5.cnzz.com/stat.php%3Fid%3D1252889774%26show%3Dpic1' type='text/javascript'%3E%3C/script%3E"));</script>
+
+    
+
+		<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "//hm.baidu.com/hm.js?e937132d7f7e86dfb5300ce1ab2c25f7";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
+
+
+
+		</div>
+</footer>
+
+<script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/js/backtop.js"></script>
+
+<?php wp_footer(); ?>
+
+
+
+</body>
+</html>
